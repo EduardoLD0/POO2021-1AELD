0acfe06c61784950f163cf377b336609d14f8d9a
P P <eduardold0000@gmail.com>
2021-02-28 23:28:45 -0500

    Crear clase triangulo con sus atributos y metodos

--- clasesC++/main.cpp
+++ clasesC++/main.cpp
@@ -137,6 +137,77 @@ void Circulo::setRadio( float radio ){
    this->radio = radio;
 }
 
+class Triangulo{
+	private:
+		float base, altura, ladoIzq, ladoDer;
+	public:
+		Triangulo();
+		Triangulo(float, float, float, float);
+		void calcularArea();
+		void calcularPerimetro();
+		float getBase();
+		float getAltura();
+		float getLadoIzq();
+		float getLadoDer();
+		void setBase(float);
+		void setAltura(float);
+		void setLadoIzq(float);
+		void setLadoDer(float);
+};
+
+Triangulo::Triangulo(){
+	
+}
+
+Triangulo::Triangulo(float base, float altura, float ladoIzq, float ladoDer){
+	this->base = base;
+	this->altura = altura;
+	this->ladoIzq = ladoIzq;
+	this->ladoDer = ladoDer;
+}
+
+void Triangulo::calcularArea(){
+	float area = base * altura / 2;
+	cout << "El area es: " << area << endl;
+}
+
+void Triangulo::calcularPerimetro(){
+	float perimetro = base + ladoIzq + ladoDer;
+	cout << "El perimetro es: " << perimetro << endl;
+}
+
+float Triangulo::getBase(){
+	return this->base;
+}
+
+float Triangulo::getAltura(){
+	return this->altura;
+}
+
+float Triangulo::getLadoIzq(){
+	return this->ladoIzq;
+}
+
+float Triangulo::getLadoDer(){
+	return this->ladoDer;
+}
+
+void Triangulo::setBase(float base){
+	this->base = base;
+}
+
+void Triangulo::setAltura(float altura){
+	this->altura = altura;
+}
+
+void Triangulo::setLadoIzq(float ladoIzq){
+	this->ladoIzq = ladoIzq;
+}
+
+void Triangulo::setLadoDer(float ladoDer){
+	this->ladoDer = ladoDer;
+}
+
 int main()
 {
    /*//Instanciar --> crear objetos de una clase
